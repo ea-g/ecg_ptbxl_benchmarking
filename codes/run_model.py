bdf2c85ec6343eaf55381e8a2b678874ebbbba30
ea-g <58582043+ea-g@users.noreply.github.com>
2021-08-14 13:22:00 +0900

    test run

--- codes/run_model.py
+++ codes/run_model.py
@@ -1,12 +1,13 @@
 from experiments.scp_experiment import SCP_Experiment
 from configs.wavelet_configs import *
+from configs.your_configs import *
 
 datafolder = '../data/ptbxl/'
 outputfolder = '../output/'
 
-models = [conf_wavelet_standard_lr]
+models = [conf_wavelet_standard_lr, conf_minirocket_standard_lr]
 
-e = SCP_Experiment('wavelet_1', 'diagnostic', datafolder, outputfolder, models)
+e = SCP_Experiment('wavelet_MR_1', 'diagnostic', datafolder, outputfolder, models)
 e.prepare()
 e.perform()
 e.evaluate()
