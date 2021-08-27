9665b725786459da7cfc8d9bf29e70d186176366
ea-g <58582043+ea-g@users.noreply.github.com>
2021-08-27 13:58:46 +0900

    superdiagnostic output

--- codes/run_model.py
+++ codes/run_model.py
@@ -7,7 +7,7 @@ outputfolder = '../output/'
 
 models = [conf_wavelet_standard_lr, conf_minirocket_standard_lr]
 
-e = SCP_Experiment('wavelet_MR_1', 'diagnostic', datafolder, outputfolder, models)
+e = SCP_Experiment('wavelet_MR_super', 'superdiagnostic', datafolder, outputfolder, models)
 e.prepare()
 e.perform()
 e.evaluate()
